27c3b47bb41c9ca1a6a797cac5da6fc5c443fafe
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-06-10 12:08:09 +0530

    use selenium manager for automation

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,3 +1,4 @@
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,8 +8,13 @@ public class OpenGoogle {
     //Go to Google home page
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver","D:\\SeliniumQA\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver","D:\\SeliniumQA\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"); //STAGE 1
+
+        //WebDriverManager.chromedriver().setup();    //STAGE 02
+
+        //In Stage 03 only need these 02 command
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");     //to redirect to URL
+        driver.quit();
     }
 }
