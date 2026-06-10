ce61f4113489a25c359190d9809addb8280c3d18
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-06-10 09:05:58 +0530

    fix: resolve ChromeDriver version mismatch and path error
    - Updated chromedriver.exe to match Chrome version 148
    - Fixed incorrect path (removed extra chromedriver-win6 folder)
    - Verified Selenium session starts successfully with W3C dialect

--- src/main/java/OpenGoogle.java
+++ src/main/java/OpenGoogle.java
@@ -1,2 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class OpenGoogle {
+
+    //Open Google
+    //Go to Google home page
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","D:\\SeliniumQA\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+    }
 }
