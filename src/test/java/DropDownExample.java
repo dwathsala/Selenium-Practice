4184b58fae31aa76b73a27873f2e91bab59bbefd
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-06-29 15:22:52 +0530

    feat: implement dropdown selection techniques in LeafGround

--- src/test/java/DropDownExample.java
+++ src/test/java/DropDownExample.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class DropDownExample {
 
     WebDriver driver;
@@ -19,17 +21,46 @@ public class DropDownExample {
 
     @Test
     public void leafGroundPageDropDown() throws InterruptedException {
+
         //1.1. Ways of select values in basic dropdown
         WebElement dropDown = driver.findElement(By.xpath("//select[@class='ui-selectonemenu']"));
-        Select select = new Select(dropDown);
-        select.selectByIndex(1);
+        Select select = new Select(dropDown);  //need to pass web element as parameter
+        select.selectByIndex(1);  //Select by visible text denwanam value kiyla ttribute ekk thiyenn oni aniwaren
         Thread.sleep(3000);
         select.selectByVisibleText("Playwright");
         Thread.sleep(3000);
 
+        //1.2. Get the number of dropdown options
+        // List<WebElement> =====> Generics
+        List<WebElement> listofOptions = select.getOptions();
+        int size = listofOptions.size();
+        System.out.println("Number of options in dropdown: "+size);
+
+        for (WebElement element:listofOptions){
+             System.out.println(element.getText());
+        }
+        //1.3. Using sendkeys selct dropdown value
+        dropDown.sendKeys("Puppeteer");
+        Thread.sleep(3000);
+
+        //1.4. Selecting value in a Boostrap dropdown
+        WebElement dropDown2 = driver.findElement(By.xpath("//div[@id='j_idt87:country']"));
+        dropDown2.click();
+        List<WebElement> listofDropDownValues = driver.findElements(By.xpath("//ul[@id='j_idt87:country_items']/li"));
+        listofDropDownValues.size();
+        System.out.println(size);
+
+        for (WebElement element : listofDropDownValues){
+            String dropDownValue = element.getText();
+            if (dropDownValue.equals("USA")){
+                element.click();
+                break;
+            }
+        }
+
     }
 
-
-
+    //2. Google Search - pick a value from suggestions
+    //3. Handle Hidden Auto Suggestions Drop Down And Search using DOM Debugger Trick
 
 }
